cde8565e0e6f1ed6cbbb2cda0401158e76418277
khaled94 <khaledmamdouh33@gmail.com>
2022-08-30 00:07:45 +0200

    trying webview

--- app/src/main/java/com/example/routerchefdemo/MainActivity.kt
+++ app/src/main/java/com/example/routerchefdemo/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.routerchefdemo
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.routerchefdemo.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -14,9 +19,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.bLogin.setOnClickListener {
-            val intent = Intent(this, RouterDataActivity::class.java)
-            startActivity(intent)
-            finish()
+            /*  val intent = Intent(this, RouterDataActivity::class.java)
+              startActivity(intent)
+              finish() */
+            val webView = WebView(this)
+            val settings = webView.settings
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.databaseEnabled = true
+            settings.mixedContentMode = 0
+            settings.builtInZoomControls = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            settings.setAppCacheEnabled(false)
+            //settings.setCacheMode(2);
+            webView.addJavascriptInterface(this, "Android");
+// run script
+            getLoginScript(binding.etUsername.text.toString(),
+                binding.etPassword.text.toString())?.let { it1 -> webView.evaluateJavascript(it1, null) };
         }
     }
+
+    fun getLoginScript(str: String?, str2: String?): String? {
+            return "let username = \",str,\";\nlet password = \",str2,;\n\nlet exit = setTimeout(() => {\n    clearInterval(temp);\n    clearTimeout(exit);\n    Android.callbackHandle(JSON.stringify({result:\"timeout\"}));    \n}, 25000);\nlet temp = setInterval(() => {\n        if (document.getElementById('login_window')) {\n            let errMsg = document.getElementById('errorCategory').innerText;\n            if (errMsg.includes(\"minute\")) {\n                clearInterval(temp);\n                clearTimeout(exit);\n            Android.callbackHandle(JSON.stringify({ result: \"retry_after\", time: 60 }));\n            }\n            else if(errMsg.includes(\"You are already logged in.\")){\n                clearInterval(temp);\n                clearTimeout(exit);\n                Android.callbackHandle(JSON.stringify({result:\"already_login\"}));\n            }\n             else {\n\n                if (document.getElementById(\"setfirstbutton\")) {\n                    Android.callbackHandle(JSON.stringify({result:\"enforce_login\"}));\n                    document.getElementById(\"setfirstbutton\").click();\n                } else {\n                    if (errMsg.includes(\"Incorrect\")) {\n                        Android.callbackHandle(JSON.stringify({result:\"invalid_login\"}));\n                    }\n                    if (document.getElementById('index_username')) {\n                        Android.callbackHandle(JSON.stringify({result:\"logging_in\"}));\n                        document.getElementById('index_username').value = username;\n                        document.getElementById('password').value = password;\n                        document.getElementById(\"loginbtn\").click();\n                    }\n                }\n            }\n        } else if (document.getElementById('wizard_wifi_title')) {\n            clearInterval(temp);\n            clearTimeout(exit);\n            Android.callbackHandle(JSON.stringify({result:\"login_success\"}));\n        }\n}, 1000);"
+
+    }
+
 }
+
--- app/src/main/java/com/example/routerchefdemo/RouterDataActivity.kt
+++ app/src/main/java/com/example/routerchefdemo/RouterDataActivity.kt
@@ -1,16 +1,42 @@
 package com.example.routerchefdemo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.webkit.JavascriptInterface
 import androidx.appcompat.app.AppCompatActivity
 import com.example.routerchefdemo.databinding.ActivityRouterDataBinding
 
+
 class RouterDataActivity : AppCompatActivity() {
+
+    object AndroidJSInterface {
+        @JavascriptInterface
+        fun onClicked() {
+            Log.d("HelpButton", "Help button clicked")
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityRouterDataBinding.inflate(layoutInflater)
         val view: View = binding.root
         setContentView(view)
-        binding.webView.loadUrl("https://www.google.com/")
+        binding.webView.loadUrl("https://192.168.1.1/")
+        binding.webView.settings.javaScriptEnabled = true
+
+      /*  binding.webView.post {
+            binding.webView.loadUrl(
+                "document.getElementById('index_username').value = admin;\n" +
+                        "                        document.getElementById('password').value = K9114659;\n" +
+                        "                        document.getElementById(\\\"loginbtn\\\").click();\n" +
+                        "[8:51 PM, 8/29/2022] Mona: if (document.getElementById('wizard_wifi_title')) \n" +
+                        "            Android.callbackHandle(JSON.stringify({result:\\\"login_success\\\"}));"
+            )
+        }*/
+
+
     }
 }
