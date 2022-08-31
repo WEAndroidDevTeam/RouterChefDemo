136e20dba3402766b322e6d05427cff4ff717afa
monawhab <monawhab@gmail.com>
2022-08-31 14:49:32 +0200

    [unfinished]

--- app/src/main/java/com/example/routerchefdemo/MainActivity.kt
+++ app/src/main/java/com/example/routerchefdemo/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.routerchefdemo
 
+import android.R
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.webkit.JavascriptInterface
+import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.routerchefdemo.databinding.ActivityMainBinding
@@ -20,11 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.bLogin.setOnClickListener {
-            /*  val intent = Intent(this, RouterDataActivity::class.java)
-              startActivity(intent)
-              finish() */
-            val webView = WebView(this)
-            val settings = webView.settings
+            val settings = binding.webView.settings
             settings.javaScriptEnabled = true
             settings.domStorageEnabled = true
             settings.databaseEnabled = true
@@ -33,18 +34,30 @@ class MainActivity : AppCompatActivity() {
             settings.loadWithOverviewMode = true
             settings.useWideViewPort = true
             settings.setAppCacheEnabled(false)
-            //settings.setCacheMode(2);
-            webView.addJavascriptInterface(this, "Android");
+//            settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
 
-            webView.loadUrl("https://192.168.1.1/")
+            binding.webView.addJavascriptInterface(this, "Android");
+//            binding.webView.evaluateJavascript(getGoogleScript(), null);
+//            binding.webView.loadUrl("script: (" + getGoogleScript() + ")")
+//            binding.webView.evaluateJavascript("javascript:(function() { document.getElementById('q').value = '\" + email + \"'; })()", null);
+
+            binding.webView.webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    binding.webView.evaluateJavascript(getLoginScript("",""), null)
+                }
+            }
+            binding.webView.loadUrl("http://192.168.1.1/")
+
 // run script
-            getLoginScript(binding.etUsername.text.toString(),
-                binding.etPassword.text.toString())?.let { it1 -> webView.evaluateJavascript(it1, null) };
         }
     }
 
-    fun getLoginScript(str: String, str2: String): String? {
-        return concatStrings("let username = \"", str, "\";\nlet password = \"", str2, "\";\n\nlet exit = setTimeout(() => {\n    clearInterval(temp);\n    clearTimeout(exit);\n    Android.callbackHandle(JSON.stringify({ result: \"timeout\" }));\n}, 25000);\nlet temp = setInterval(() => {\n    try {\n        if (document.getElementById(\"login_error_waittime\") && document.getElementById(\"login_error_waittime\").value > 1) {\n            let seconds = document.getElementById(\"login_error_waittime\").value;\n            clearInterval(temp);\n            clearTimeout(exit);\n            Android.callbackHandle(JSON.stringify({ result: \"retry_after\", time: parseInt(seconds) }));\n        }\n        else {\n            if ([...document.getElementsByTagName(\"input\")].filter(input => input.value == \"Skip\")[0]) {\n                [...document.getElementsByTagName(\"input\")].filter(input => input.value == \"Skip\")[0].click();\n                Android.callbackHandle(JSON.stringify({ result: \"enforce_login\" }));\n            }\n            else if (document.getElementById(\"NewPassword\")) {\n                document.getElementById(\"Btn_cancel\").click();\n            }\n            else if (document.getElementById('login_error_span')) {\n\n                if (document.getElementById('Frm_Username')) {\n                    Android.callbackHandle(JSON.stringify({ result: \"logging_in\" }));\n                    document.getElementById('Frm_Username').value = username;\n                    document.getElementById('Frm_Password').value = password;\n                    document.getElementById('LoginId').click();\n                }\n                if (document.getElementById('login_error_span').innerText.includes(\"error.\")) {\n                    Android.callbackHandle(JSON.stringify({ result: \"invalid_login\" }));\n                }\n\n            } else if (document.getElementById(\"radio1\")) {\n                Android.callbackHandle(JSON.stringify({ result: \"enforce_login\" }));\n                document.getElementById(\"radio1\").checked = true;\n                document.getElementById(\"Btn_apply\").click();\n            } else if (document.getElementById(\"Btn_Finish\")) {\n                document.getElementById(\"Btn_Finish\").click();\n            } else if (document.getElementById(\"Btn_Next\")) {\n                Android.callbackHandle(JSON.stringify({ result: \"enforce_login\" }));\n                document.getElementById(\"Btn_Next\").click();\n            } else if (document.getElementById('WANUrl')) {\n                clearInterval(temp);\n                clearTimeout(exit);\n                Android.callbackHandle(JSON.stringify({ result: \"login_success\" }));\n            }\n        }\n    } catch (err){ }\n}, 500);")
+    fun getLoginScript(str: String, str2: String): String {
+        return concatStrings("script: let username = \"", str, "\";\nlet password = \"", str2, "\";\n\nlet exit = setTimeout(() => {\n    clearInterval(temp);\n    clearTimeout(exit);\n    Android.callbackHandle(JSON.stringify({ result: \"timeout\" }));\n}, 25000);\nlet temp = setInterval(() => {\n    try {\n        if (document.getElementById(\"login_error_waittime\") && document.getElementById(\"login_error_waittime\").value > 1) {\n            let seconds = document.getElementById(\"login_error_waittime\").value;\n            clearInterval(temp);\n            clearTimeout(exit);\n            Android.callbackHandle(JSON.stringify({ result: \"retry_after\", time: parseInt(seconds) }));\n        }\n        else {\n            if ([...document.getElementsByTagName(\"input\")].filter(input => input.value == \"Skip\")[0]) {\n                [...document.getElementsByTagName(\"input\")].filter(input => input.value == \"Skip\")[0].click();\n                Android.callbackHandle(JSON.stringify({ result: \"enforce_login\" }));\n            }\n            else if (document.getElementById(\"NewPassword\")) {\n                document.getElementById(\"Btn_cancel\").click();\n            }\n            else if (document.getElementById('login_error_span')) {\n\n                if (document.getElementById('Frm_Username')) {\n                    Android.callbackHandle(JSON.stringify({ result: \"logging_in\" }));\n                    document.getElementById('Frm_Username').value = username;\n                    document.getElementById('Frm_Password').value = password;\n                    document.getElementById('LoginId').click();\n                }\n                if (document.getElementById('login_error_span').innerText.includes(\"error.\")) {\n                    Android.callbackHandle(JSON.stringify({ result: \"invalid_login\" }));\n                }\n\n            } else if (document.getElementById(\"radio1\")) {\n                Android.callbackHandle(JSON.stringify({ result: \"enforce_login\" }));\n                document.getElementById(\"radio1\").checked = true;\n                document.getElementById(\"Btn_apply\").click();\n            } else if (document.getElementById(\"Btn_Finish\")) {\n                document.getElementById(\"Btn_Finish\").click();\n            } else if (document.getElementById(\"Btn_Next\")) {\n                Android.callbackHandle(JSON.stringify({ result: \"enforce_login\" }));\n                document.getElementById(\"Btn_Next\").click();\n            } else if (document.getElementById('WANUrl')) {\n                clearInterval(temp);\n                clearTimeout(exit);\n                Android.callbackHandle(JSON.stringify({ result: \"login_success\" }));\n            }\n        }\n    } catch (err){ }\n}, 500);")
+    }
+    fun getGoogleScript(): String {
+        return "script: document.getElementById('q').value = 'e'; document.getElementById('btnK').click();"
     }
 
     @JavascriptInterface
