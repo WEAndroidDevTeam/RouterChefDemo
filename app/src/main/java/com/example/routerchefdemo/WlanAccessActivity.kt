1375bed7ddb4a224df9b54758d83eb9e6dcf2a6b
mohey.ezat <https://github.com/WEAndroidDevTeam/My_WE.git>
2023-09-10 13:27:34 +0200

    WLANAccess UI

---
+++ app/src/main/java/com/example/routerchefdemo/WlanAccessActivity.kt
@@ -0,0 +1,11 @@
+package com.example.routerchefdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WlanAccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wlan_access)
+    }
+}
