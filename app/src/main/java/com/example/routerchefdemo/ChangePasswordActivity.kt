2717e556c9f8b7e244ecc3bfdf6e895e0ec318df
monawhab <monawhab@gmail.com>
2023-09-17 15:13:09 +0300

    Change Password

---
+++ app/src/main/java/com/example/routerchefdemo/ChangePasswordActivity.kt
@@ -0,0 +1,40 @@
+package com.example.routerchefdemo
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.routerchefdemo.databinding.ActivityChangePasswordBinding
+
+class ChangePasswordActivity : BaseActivity<ActivityChangePasswordBinding>() {
+    override fun getViewBinding() = ActivityChangePasswordBinding.inflate(layoutInflater)
+    override fun setCurrentActivity() = (applicationContext as MyApp).setCurrentActivity(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view: View = binding.root
+
+        setContentView(view)
+        Constants.webview.loadUrl("https://192.168.1.1/html/wizard/wifi.html")
+
+        binding.bApply.setOnClickListener {
+            Constants.webview.evaluateJavascript("javascript: " +
+                    "document.querySelector('button#wifi_wizard_save.atp_button.fontweight_thick').addEventListener('click', function(e){" +
+                    "document.querySelector('#home_wifi_access24id_ctrl').addEventListener('change', function(){" +
+                    "document.querySelector('#home_wifi_access24id_ctrl').value = '${binding.etSsid.text.toString()}';" +
+                    "});" +
+                    "document.querySelector('#hidesharekeyMenu_Password_ctrl').addEventListener('change', function(){" +
+                    "document.querySelector('#hidesharekeyMenu_Password_ctrl').value = '${binding.etPassword.text.toString()}';" +
+                    "});" +
+                    "document.querySelector('#home_wifi_access24id_ctrl').dispatchEvent(new Event('change', {'bubbles': true}));" +
+                    "document.querySelector('#hidesharekeyMenu_Password_ctrl').dispatchEvent(new Event('change', {'bubbles': true}));" +
+                    "});" +
+                    "document.querySelector('button#wifi_wizard_save.atp_button.fontweight_thick').click();" +
+                    "Android.callbackHandle('change password' , 'success');", null)
+        }
+    }
+
+    override fun render(str: String, data: String) {
+        Toast.makeText(this, "dataa $data", Toast.LENGTH_LONG).show()
+    }
+
+}
