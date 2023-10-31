d82f3c3c0aa92c07f50c6cbfa4bd6df8ed52a84b
monawhab <monawhab@gmail.com>
2023-10-31 08:40:08 +0200

    [Uncompleted] Change SSID Name

--- app/src/main/java/com/example/routerchefdemo/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/routerchefdemo/ChangePasswordActivity.kt
@@ -9,7 +9,7 @@ import com.example.routerchefdemo.databinding.ActivityChangePasswordBinding
 class ChangePasswordActivity : BaseActivity<ActivityChangePasswordBinding>() {
     override fun getViewBinding() = ActivityChangePasswordBinding.inflate(layoutInflater)
     override fun setCurrentActivity() = (applicationContext as MyApp).setCurrentActivity(this)
-
+var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view: View = binding.root
@@ -21,73 +21,45 @@ class ChangePasswordActivity : BaseActivity<ActivityChangePasswordBinding>() {
 
         binding.bApply.setOnClickListener {
             binding.progressCircular.visibility = View.VISIBLE
+            if(count == 1) {
+                Constants.webview.evaluateJavascript(
+                    "javascript: " +
+                            "function handleWifiSettings() {" +
+                            "let applied = false;" +
+
+                            "let ssid = '${binding.etSsid.text.toString()}';" +
+                            "let password = '${binding.etPassword.text.toString()}';" +
+                            "let hidden = false;" +
+                            "let open = false;" +
+
+                            "let maxClients = 28;" +
+
+                            "                    document.getElementById('ESSID:0').value = ssid;" +
+                            "                        document.getElementById('Btn_apply_WLANSSIDConf:0').click();" +
+                            "console.log('clickkkkkkkkked');" +
+                            "}" +
+                            "handleWifiSettings();", null
+                )
+                return@setOnClickListener
+            }
+            count ++
+
             Constants.webview.evaluateJavascript("javascript: " +
                     "function handleWifiSettings() {" +
                     "let applied = false;" +
-                    
+
                     "let ssid = '${binding.etSsid.text.toString()}';" +
                     "let password = '${binding.etPassword.text.toString()}';" +
                     "let hidden = false;" +
                     "let open = false;" +
-                    
+
                     "let maxClients = 28;" +
-                    "    let exit = setTimeout(() => {" +
-                    "        clearInterval(temp);" +
-                    "        clearTimeout(exit);" +
-                    "        Android.callbackHandle('${Constants.CHANGE_PASSWORD}' , 'relogin');" +
-                    "    }, 10000);" +
-                    
-                    "    let temp = setInterval(() => {" +
-                    "        try {" +
-                    "            if (document.getElementsByClassName('emFont loginTitle')[0]) {" +
-                    "                clearInterval(temp);" +
-                    "                clearTimeout(exit);" +
-                    "                Android.callbackHandle('${Constants.CHANGE_PASSWORD}' , 'relogin');" +
-                    "            } else if (document.getElementById('WLANSSIDConf_container').style.display === 'none') {" +
+
                     "                document.getElementById('WLANSSIDConfBar').click();" +
-                    "                Android.callbackHandle('${Constants.CHANGE_PASSWORD}' , 'wait until success');" +
-                    "            } else {" +
-                    "                if (document.getElementById('confirmLayer').style.display !== 'none') {" +
-                    "                    document.getElementById('confirmOK').click();" +
-                    "                    clearInterval(temp);" +
-                    "                    clearTimeout(exit);" +
-                    "                    setTimeout(() => {" +
-                    "                        Android.callbackHandle('${Constants.CHANGE_PASSWORD}' , 'wait until success');" +
-                    "                    }, 1000);" +
-                    "                } else if (applied) {" +
-                    "                    clearInterval(temp);" +
-                    "                    clearTimeout(exit);" +
-                    "                    setTimeout(() => {" +
-                    "                        Android.callbackHandle('${Constants.CHANGE_PASSWORD}' , 'wait until success');" +
-                    "                    }, 1000);" +
-                    "                } else if (!document.getElementById('KeyPassphrase:0').value.includes('/t') && document.getElementById('KeyPassphrase:0').value !== '\\t\\t\\t\\t\\t\\t') {" +
-                    "                    document.getElementById('ESSID:0').value = ssid;" +
-                    "                    if (hidden) {" +
-                    "                        document.getElementById('ESSIDHideEnable0:0').checked = true;" +
-                    "                    } else {" +
-                    "                        document.getElementById('ESSIDHideEnable1:0').checked = true;" +
-                    "                    }" +
-                    "                    if (!open) {" +
-                    "                        document.getElementById('EncryptionType:0').value = 'WPA/WPA2-PSK-TKIP/AES';" +
-                    "                        if (password) {" +
-                    "                            document.getElementById('KeyPassphrase:0').value = password;" +
-                    "                        }" +
-                    "                    } else {" +
-                    "                        document.getElementById('EncryptionType:0').value = 'No Security';" +
-                    "                    }" +
-                    "                    document.getElementById('MaxUserNum:0') && (document.getElementById('MaxUserNum:0').value = maxClients);" +
-                    "                    if (document.getElementById('Btn_apply_WLANSSIDConf:0')) {" +
-                    "                        document.getElementById('Btn_apply_WLANSSIDConf:0').click();" +
-                    "                        applied = true;" +
-                    "                    }" +
-                    "                } else {" +
-                    "                    document.getElementById('Switch_KeyPassType:0').click();" +
-                    "                }" +
-                    "            }" +
-                    "        } catch (err) { }" +
-                    "    }, 1000);" +
-                    "}"
-                    , null)
+                    "console.log('loadeddd');" +
+                    "}" +
+                    "handleWifiSettings();"
+                , null)
         }
     }
 
